f0678bc5510bbe94a03247ba33e6df056d765b9e
Logan <l22bitt1@gmail.com>
2025-11-20 17:48:20 -0500

    maybe?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site:'https://Logs22.github.io',
+  site:'https://Logs22.github.io/Portfolio/',
   base:'/Portfolio/',
 });
